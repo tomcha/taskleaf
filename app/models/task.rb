de45b44896e49d88521ee44d1108e489e2e55956
tomcha <tomcha@tomcha.net>
2018-11-06 16:45:19 +0900

    add original validate

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,9 @@
 class Task < ApplicationRecord
-  validates :name, presence: true
-  validates :name, length: { maximum: 30 }
+  validates :name, presence: true, length: { maximum: 30}
+  validate :validate_name_not_including_comma
+
+  private
+  def validate_name_not_including_comma
+    errors.add(:name, 'にコンマを含めることはできません') if name&.include?(',')
+  end
 end
