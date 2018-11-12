91f1094f596956ea07fe564e2829378fea998ffc
tomcha <tomcha@tomcha.net>
2018-11-12 14:58:00 +0900

    強制ログイン機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
+
+  def login_required
+    redirect_to login_path unless current_user
+  end
 end
