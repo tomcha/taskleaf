a73558d2220796534d2285e6aeea3423dff0ccd6
tomcha <tomcha@tomcha.net>
2018-11-12 15:24:52 +0900

    ユーザーとタスクを1対多でリレーション定義を追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,8 @@ class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30}
   validate :validate_name_not_including_comma
 
+  belongs_to :user
+
   private
   def validate_name_not_including_comma
     errors.add(:name, 'にコンマを含めることはできません') if name&.include?(',')
